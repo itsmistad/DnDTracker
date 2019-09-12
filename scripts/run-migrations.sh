a084ee288c31069d4db0156041b586b7df73ab17
Derek <d@mistad.net>
2019-09-11 21:01:38 -0500

    Separated run-migrations.sh and Travis

--- scripts/run-migrations.sh
+++ scripts/run-migrations.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
-if [[ -z "$TRAVIS_PULL_REQUEST" ]] || [[ $TRAVIS_PULL_REQUEST == “false” ]]; then
-    dotnet build ../src/DnDTracker.Migrations/DnDTracker.Migrations.csproj -c Release
-    dotnet ../src/DnDTracker.Migrations/bin/Release/netcoreapp2.2/DnDTracker.Migrations.dll "$1"
-fi
+dotnet build ../src/DnDTracker.Migrations/DnDTracker.Migrations.csproj -c Release
+dotnet ../src/DnDTracker.Migrations/bin/Release/netcoreapp2.2/DnDTracker.Migrations.dll "$1"
