63860dd963293f38596c878295d074bd35d3ea02
Derek Williamson <d@mistad.net>
2019-09-18 17:12:49 -0500

    Added standardized header, footer, and content background

--- src/DnDTracker.Web/Controllers/HomeController.cs
+++ src/DnDTracker.Web/Controllers/HomeController.cs
@@ -5,13 +5,10 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 
-// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
-
 namespace DnDTracker.Web.Controllers
 {
     public class HomeController : Controller
     {
-        // GET: /<controller>/
         public IActionResult Index()
         {
             return View();
