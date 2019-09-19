4efd3facf86d0b36537ddf9a3adf9a6840aa8f89
Derek Williamson <d@mistad.net>
2019-09-18 21:55:54 -0500

    Added placeholders for About and Login
    Added a script for container resizing #main-content

---
+++ src/DnDTracker.Web/wwwroot/js/theme.js
@@ -0,0 +1,14 @@
+$(function () {
+    var headerBarHeight = $('#main-header').height();
+    var footerBarHeight = $('#main-footer').height();
+    var resize = function () {
+        var contentContainerHeight = $('#main-content').height();
+        var totalHeight = $(document).innerHeight();
+        var fillHeight = totalHeight - headerBarHeight - footerBarHeight;
+        if (contentContainerHeight < fillHeight)
+            $('#main-content').css('height', `${fillHeight}px`);
+    }
+    $(window).on('resize', resize);
+
+    resize();
+});
