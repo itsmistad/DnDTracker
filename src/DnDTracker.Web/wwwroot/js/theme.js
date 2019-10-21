d036773ed0e090fcad02d28e3fa292c632371537
Derek Williamson <d@mistad.net>
2019-10-21 17:16:24 -0500

    Finished template for character creation page

--- src/DnDTracker.Web/wwwroot/js/theme.js
+++ src/DnDTracker.Web/wwwroot/js/theme.js
@@ -1,11 +1,13 @@
 var headerBarHeight = $('#main-header').height();
+var taskBarHeight = $('#main-taskbar').length ? $('#main-taskbar').height() : 0;
 var footerBarHeight = $('#main-footer').height();
 var resize = function () {
     var contentContainerHeight = $('#main-content').height();
     var totalHeight = $(document).innerHeight();
-    var fillHeight = totalHeight - headerBarHeight - footerBarHeight - 1;
-    if (contentContainerHeight < fillHeight)
-        $('#main-content').css('height', `${fillHeight}px`);
+    var fillHeight = totalHeight - headerBarHeight - taskBarHeight - footerBarHeight - 1;
+    if (contentContainerHeight <= fillHeight) {
+        $('#main-content').css('min-height', `${fillHeight}px`);
+    }
 }
 resize();
 
